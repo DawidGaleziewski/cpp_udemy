e7f9cbc5087980a3c731a3034b14f5a1146951c6
dawid <dawid.galeziewski@gmail.com>
2023-08-27 00:48:06 +0200

    saving notes

--- workspaces/7_functions/1_basics/main.cpp
+++ workspaces/7_functions/1_basics/main.cpp
@@ -22,6 +22,15 @@ void pass_by_value(vector<string> name_list){
     cout << "Inside fn name_list 2nd item is: " << name_list.at(2) << endl; // ass cpp functions are passed by value we will not mutate anything outside this function;
 }
 
+void false_pass_by_value(int numbers[]){ // arrays are exception they WILL NOT GET COPIED. we will pass a referance of first element of the array
+    numbers[2] = 69; // THIS WILL GET MUTATED!
+}
+
+// we can guard ourselves from not intentionally overriding arrays by using const in param declaration
+void guard_from_my_stupidity(const int nums[]){
+//    nums[2] = 69; // would throw a compiler error
+};
+
 int main(int argc, char **argv)
 {
     
@@ -33,5 +42,9 @@ int main(int argc, char **argv)
     vector<string> original_list {"Mark", "John", "Andrew"};
     pass_by_value(original_list);
     cout << "original list 2nd item is: " << original_list.at(2) << endl;
+    
+    int original_numbers[] {0,1,2};
+    false_pass_by_value(original_numbers);
+    cout << "2nd element of original_numbers is now: " << original_numbers[2] << endl; // THIS WILL GET MUTATED
 	return 0;
 }
